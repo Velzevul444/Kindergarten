edf88f2937f6ef23fc1574e4a329af094feac5c2
Velzevul <maximilianus06@gmail.com>
2026-09-17 06:54:19 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import classes.Enrollment;
 import classes.Parents;
 import Enumes.Status;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -57,7 +58,14 @@ enrollments.add(new Enrollment(
             System.out.println("4. Search by date");
             System.out.println("5. Filter by status");
             System.out.println("6. Filter by date range");
-            System.out.println("7. Exit");
+            System.out.println("7. Statistic");
+            System.out.println("8. Sort by nameChild");
+            System.out.println("9. Sort by date");
+            System.out.println("10. Create enrollment");
+            System.out.println("11. Read enrollment");
+            System.out.println("12. Update enrollment");
+            System.out.println("13. Delete enrollment");
+            System.out.println("14. Exit");
             System.out.print("Enter your choice: ");  
 
             while (!scanner.hasNextInt()) {
@@ -171,12 +179,213 @@ enrollments.add(new Enrollment(
                         }
                     break;
                 case 7:
+                    int newCount = 0;
+                    int reviewCount = 0;
+                    int approvedCount = 0;
+                    int rejectedCount = 0;
+                    int enrolledCount = 0;
+                    int cancelledCount = 0;
+
+                    for (Enrollment enrollment : enrollments){
+                        switch (enrollment.getStatus()) {
+                            case New:
+                                newCount++;
+                                break;
+                            case Review:
+                                reviewCount++;
+                                break;
+                            case Approved:
+                                approvedCount++;
+                                break;
+                            case Rejected:
+                                rejectedCount++;
+                                break;
+                            case Enrolled:
+                                enrolledCount++;
+                                break;
+                            case Cancelled:
+                                cancelledCount++;
+                                break;
+        }
+                    }  
+                    System.out.println("Total parents: " + parents.size());
+                    System.out.println("Total enrollments: " + enrollments.size());
+                    System.out.println("New applications: " + newCount);
+                    System.out.println("Under review: " + reviewCount);
+                    System.out.println("Approved applications: " + approvedCount);
+                    System.out.println("Rejected applications: " + rejectedCount);
+                    System.out.println("Enrolled children: " + enrolledCount);
+                    System.out.println("Cancelled applications: " + cancelledCount);
+    
+                    break;
+                case 8:
+                    enrollments.sort(
+                        Comparator.comparing(Enrollment::getChildName)
+                    );
+
+                    for (Enrollment enrollment : enrollments) {
+                        System.out.println("Enrollment ID: " + enrollment.getId());
+                        System.out.println("Child Name: " + enrollment.getChildName());
+                        System.out.println("Created At: " + enrollment.getCreatedAt());
+                        System.out.println("Status: " + enrollment.getStatus());
+                    }
+                    break;
+                case 9: 
+                    enrollments.sort(
+                        Comparator.comparing(Enrollment::getCreatedAt)
+                    );
+
+                    for (Enrollment enrollment : enrollments) {
+                        System.out.println("Enrollment ID: " + enrollment.getId());
+                        System.out.println("Child Name: " + enrollment.getChildName());
+                        System.out.println("Created At: " + enrollment.getCreatedAt());
+                        System.out.println("Status: " + enrollment.getStatus());
+                    }
+                    break;   
+                case 10: {
+                    scanner.nextLine();
+
+                    System.out.print("Enter child name: ");
+                    String childName = scanner.nextLine();
+
+                    System.out.print("Enter parent ID: ");
+                    int parentId = scanner.nextInt();
+
+                    boolean parentExists = false;
+
+                    for (Parents parent : parents) {
+                        if (parent.getId() == parentId) {
+                            parentExists = true;
+                            break;
+                        }
+                    }
+
+                    if (!parentExists) {
+                        System.out.println("Parent not found.");
+                        break;
+                    }
+
+                    int newId = enrollments.size() + 1;
+
+                    Enrollment newEnrollment = new Enrollment(
+                        newId,
+                        childName,
+                        parentId,
+                        LocalDate.now(),
+                        Status.New
+                    );
+
+                    enrollments.add(newEnrollment);
+
+                    System.out.println("Enrollment created successfully.");
+                    break;
+                }
+                case 11: {
+                    System.out.print("Enter enrollment ID: ");
+
+                    while (!scanner.hasNextInt()) {
+                        System.out.println("ID must be a number.");
+                        scanner.next();
+                    }
+
+                    int enrollmentId = scanner.nextInt();
+                    boolean found = false;
+
+                    for (Enrollment enrollment : enrollments) {
+                        if (enrollment.getId() == enrollmentId) {
+                            System.out.println("Enrollment ID: " + enrollment.getId());
+                            System.out.println("Child Name: " + enrollment.getChildName());
+                            System.out.println("Parent ID: " + enrollment.getParentId());
+                            System.out.println("Created At: " + enrollment.getCreatedAt());
+                            System.out.println("Status: " + enrollment.getStatus());
+
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Enrollment not found.");
+                    }
+                    break;
+                }    
+                case 12: {
+                    System.out.print("Enter enrollment ID: ");
+
+                    while (!scanner.hasNextInt()) {
+                        System.out.println("ID must be a number.");
+                        scanner.next();
+                    }
+
+                    int enrollmentId = scanner.nextInt();
+                    scanner.nextLine();
+
+                    Enrollment selectedEnrollment = null;
+
+                    for (Enrollment enrollment : enrollments) {
+                        if (enrollment.getId() == enrollmentId) {
+                            selectedEnrollment = enrollment;
+                            break;
+                        }
+                    }
+
+                    if (selectedEnrollment == null) {
+                        System.out.println("Enrollment not found.");
+                    break;
+                    }
+
+                    System.out.print("Enter new child name: ");
+                    String newChildName = scanner.nextLine();
+                    System.out.println("Enter new status: New, Review, Approved, Rejected, Enrolled, Cancelled");
+                    String newStatus = scanner.nextLine();
+
+                    try {
+                        Status statusUpdate = Status.valueOf(newStatus);
+
+                        selectedEnrollment.setChildName(newChildName);
+                        selectedEnrollment.setStatus(statusUpdate);
+                        System.out.println("Enrollment updated successfully.");
+    
+                    } catch (IllegalArgumentException error) {
+                        System.out.println("Invalid status.");
+                    }
+
+                    break;
+                }
+                case 13: {
+                    System.out.print("Enter enrollment ID: ");
+
+                    while (!scanner.hasNextInt()) {
+                        System.out.println("ID must be a number.");
+                        scanner.next();
+                    }
+
+                    int enrollmentId = scanner.nextInt();
+                    Enrollment enrollmentDelete = null;
+
+                for (Enrollment enrollment : enrollments) {
+                    if (enrollment.getId() == enrollmentId) {
+                        enrollmentDelete = enrollment;
+                    break;
+                    }
+                }
+
+                if (enrollmentDelete == null) {
+                    System.out.println("Enrollment not found.");
+                    break;
+                }
+
+                enrollments.remove(enrollmentDelete);
+                System.out.println("Enrollment deleted successfully.");
+                break;
+                }
+                case 14:
                     System.out.println("Exit");
                 break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
-        } while (choice != 7);
+        } while (choice != 14);
             scanner.close();        
                     
         }}
